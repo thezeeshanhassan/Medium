fddae3d72c51ac4fbe6bf16e227bf5837b9d88c6
thezeeshanhassan <thezeeshanhassan@gmail.com>
2024-10-24 10:54:22 +0500

    Basic Routes Added

---  backend/src/index.ts
+++  backend/src/index.ts
@@ -6,4 +6,28 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('api/v1/user/signup', (c) => {
+  return c.text("Signup")
+})
+app.post('api/v1/user/signin', (c) => {
+  return c.text("Signin")
+})
+
+app.post('api/v1/blog', (c) => {
+  return c.text("Blog")
+})
+
+app.put('api/v1/blog/:id', (c) => {
+  return c.text("Update")
+})
+
+app.get('api/v1/blog/:id', (c) => {
+  return c.text("Get Specific Blog")
+})
+app.get('api/v1/blog/bulk', (c) => {
+  return c.text("Get All Blogs")
+})
+
+
+
 export default app
