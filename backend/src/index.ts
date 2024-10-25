c4dade7d1928c669a4d38bfb77b7cfdd6a06545c
thezeeshanhassan <thezeeshanhassan@gmail.com>
2024-10-25 09:47:20 +0500

    JWT SECRET Added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,88 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/extension'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+//Middleware For Blogs
+app.use('/api/v1/blog/*', async (c, next) => {
+
+  const header = c.req.header("authorization") || " "
+  const token = header.split(" ")[1]
+  //@ts-ignore
+  const response = await verify(token, c.env.JWT_SECRET)
+  if (response.id) {
+    next();
+  }
+  else {
+    c.status(403)
+    return c.json({
+      message: "Unauthorized"
+    })
+  }
 })
 
-app.post('api/v1/user/signup', (c) => {
-  return c.text("Signup")
+
+
+app.get('/', async (c) => {
+  return c.text("Working")
 })
-app.post('api/v1/user/signin', (c) => {
-  return c.text("Signin")
+
+app.post('api/v1/user/signup', async (c) => {
+  const prisma = new PrismaClient({
+    //@ts-ignore // To Ignore TS Errors
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password
+    }
+  })
+
+  if (!user) {
+    return c.json({
+      message: "User Does not Exists"
+    })
+  }
+
+  //@ts-ignore
+  const token = user.sign({ id: user.id }, c.env.JWT_SECRET)
+
+  return c.json({
+    token
+  })
+
+})
+app.post('api/v1/user/signin', async (c) => {
+  const prisma = new PrismaClient({
+    //@ts-ignore // To Ignore TS Errors
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password
+    }
+  })
+
+  if (!user) {
+    return c.json({
+      message: "User Does not Exists"
+    })
+  }
+
+  //@ts-ignore
+  const token = user.sign({ id: user.id }, c.env.JWT_SECRET)
+
+  return c.json({
+    token
+  })
 })
 
 app.post('api/v1/blog', (c) => {
